51f0093bc5c2b80174a10fefe0e6d43bd416c664
Pradhumn Kamble <132389587+PradhumnKamble@users.noreply.github.com>
2024-03-05 01:37:06 +0530

    Updated server-config.js

--- Flight-Booking-Service/src/config/server-config.js
+++ Flight-Booking-Service/src/config/server-config.js
@@ -1,17 +1,8 @@
-const dotenv = require("dotenv"); // config funct loads .env mapping into process.env
-// ../ --> src
-// ../../ -> root folder 
+const dotenv = require("dotenv"); 
 
-// find-config recurrs to parent dirs till it find the file
-// const res = dotenv.config({path: require('find-config')('.env')}) ; 
-
-// need to give rel path to curr dir as index.js from src is going to run
-// give rel path to that
 const res = dotenv.config({path: "./.env"}) ;
 
 if(res.error) console.log(res.error)
-// console.log(res.parsed)
-// console.log(process.cwd());
 
 module.exports = {
   PORT: process.env.PORT,
