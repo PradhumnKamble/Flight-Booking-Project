70741ac3d25484cf9b4fc45e4dcd6349a50696f7
Pradhumn Kamble <132389587+PradhumnKamble@users.noreply.github.com>
2024-03-05 01:34:36 +0530

    Updated index.js

--- Flight-Booking-Service/src/index.js
+++ Flight-Booking-Service/src/index.js
@@ -1,21 +1,12 @@
 const express = require("express");
 const apiRoutes = require("./routes") ;
-// this file contains basic server configurations
-
-const { ServerConfig } = require("./config/index.js"); // no need to add index.js
+const { ServerConfig } = require("./config/index.js");
 
 const app = express();
-app.use(express.json()); //==> will help to parse th incoming json data (only)
-// without it express wont be able to parse the data of type json
-// express.json() ==> returns a middleware that parses json data 
+app.use(express.json());
 
-app.use(express.urlencoded({extended:true})) ; // parses econded things in url 
+app.use(express.urlencoded({extended:true})) ;
 
-
-
-// 'apiRoutes' router will imposed on url conating /api as a part
-// if any point the url has /api , then we will bind apiRoutes to it
-// the return will be handled by apiRoutes
 app.use('/api' , apiRoutes) ;
 
 app.listen(ServerConfig.PORT, () => {
